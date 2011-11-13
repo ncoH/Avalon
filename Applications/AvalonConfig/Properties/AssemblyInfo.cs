2001eb9aaecbf4c569421f7bbc943680c506b677
ncoH <nico.hartmann@online.de>
2011-11-13 12:11:50 +0100

    updated danish translation (thx 1stdead!) updated version numbers of plugins

--- Applications/AvalonConfig/Properties/AssemblyInfo.cs
+++ Applications/AvalonConfig/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using MediaPortal.Common.Utils;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.6.2")]
-[assembly: AssemblyFileVersion("0.9.6.2")]
+[assembly: AssemblyVersion("0.9.7.0")]
+[assembly: AssemblyFileVersion("0.9.7.0")]
